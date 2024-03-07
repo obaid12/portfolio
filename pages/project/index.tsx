2915a6701141583898c1b4b0e02a9654cb3d8cab
Obaid ur Rehman <obaid.urrehman@systemsltd.com>
2024-03-07 19:45:52 +0500

    changes done

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,55 +1,49 @@
-import Title from '@/components/Title';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-// import { GiSkills } from 'react-icons/gi';
+import Title from "@/components/Title";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 interface Project {
-    id: number;
-    name: string;
-
+  id: number;
+  name: string;
 }
 
 const Project = () => {
-    const [projects, setProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
 
-    useEffect(() => {
-        const getProjects = async () => {
-            try {
-                const response = await fetch("/api/projects");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch projects");
-                }
-                const projectsData = await response.json();
-                setProjects(projectsData);
-            } catch (error) {
-                console.error('Error fetching projects:', error);
-            }
-        };
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const response = await fetch("/api/projects");
+        if (!response.ok) {
+          throw new Error("Failed to fetch projects");
+        }
+        const projectsData = await response.json();
+        setProjects(projectsData);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
 
-        getProjects();
-    }, []);
+    getProjects();
+  }, []);
 
-    return (
-        <div className='wrapper'>
-            <Title text='Projects' icon={undefined} />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {projects.map((project) => (
-                    <Link key={project.id} href={`/project/${project.id}`}>
-                        <div className="bg-white rounded-lg shadow-md p-4 cursor-pointer">
-                            <p className="text-gray-600">{project.name}</p>
-                        </div>
-                    </Link>
-                ))}
+  return (
+    <div className="wrapper">
+      <Title text="Projects" icon={undefined} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {projects.map((project) => (
+          <Link key={project.id} href={`/project/${project.id}`}>
+            <div className="bg-white rounded-lg shadow-md p-4 cursor-pointer">
+              <p className="text-gray-600">{project.name}</p>
             </div>
-        </div>
-    );
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
-
-
-
-
 
 // export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 //     const id = query.pid;
@@ -60,12 +54,9 @@ export default Project;
 //     const getProject = projects.find((project: { id: string | string[] | undefined; })=>project.id ==id);
 //     console.log('getProject',getProject);
 
-
 //     return {
 //         props: {
 //             project:getProject
 //         }
 //     };
 // };
-
-
